4c83c44c95a1d275edf28a9f6f52f76b3ef26671
Mauricio Pérez <mauricio.perezhv@gmail.com>
2021-01-11 00:59:01 -0300

    Primer Comit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Aplicacion de comandos
+
+Este es un ejercicio del Curso del NodeJs
+
+Recuerden instalar los paquetes de node asi:
+
+```
+npm install
+
+```
---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado una tarea'
+};
+
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualizar el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Borrar un elemento por hacer', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
